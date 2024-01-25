919e0233c95b061354b2c30441d8ad3c30d5ec6e
Loren Gordon <lorengordon@users.noreply.github.com>
2024-01-25 14:39:29 -0800

    Uses random id for test resources and allows ddb deletion

--- tests/create_all/main.tf
+++ tests/create_all/main.tf
@@ -2,10 +2,23 @@ module "state_bucket" {
   source = "../../"
 
   backend_config = {
-    bucket = "test-bucket-for-backend"
+    bucket = "test-bucket-for-backend-${local.id}"
 
     dynamodb_table = {
-      name = "test-ddb-for-backend"
+      name = "test-ddb-for-backend-${local.id}"
+
+      deletion_protection_enabled = false
     }
   }
+}
+
+resource "random_string" "this" {
+  length  = 8
+  upper   = false
+  special = false
+  numeric = false
+}
+
+locals {
+  id = random_string.this.result
 }
